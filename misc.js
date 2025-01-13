edcf78fe7a6b21f2b67346a0ec9548fb045c36b7
jcha40 <sungsoocha97@gmail.com>
2025-01-13 14:31:30 -0500

    Automatically scroll to selected button

--- misc.js
+++ misc.js
@@ -9,5 +9,6 @@ const colorMap = {
 const selectImg = function() {
     d3.select("#img").attr("src", imgAssignments[imgIndex].img);
     d3.select("#button-row").selectAll("rect").data(imgAssignments).join("rect")
-        .attr("stroke", d => d.idx === imgIndex ? "black" : "none")
+        .attr("stroke", d => d.idx === imgIndex ? "black" : "none");
+    document.getElementById("img-button-" + imgIndex).scrollIntoView();
 }
